f25245475b09488e482202cce9182ff122d70746
sumathi <sumathi@rocketmail.com>
2018-05-08 10:01:52 -0400

    Burger App, styling in progress

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,25 @@
 //setup MYSQL connection
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    port:3306,
-    host: "localhost",
-    user:"new_user",
-    password:'',
-    database:'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "new_user",
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
 //Make connection
-connection.connect( function(err) {
-    if(err) {
-        console.error("error connecting: "+err.stack);
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
         return;
     }
-    console.log('connect as id '+connection.threadId )
+    console.log('connect as id ' + connection.threadId)
 });
 
 //Export connection for our ORM to use
